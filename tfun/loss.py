fixed normalizaton bug in softmax log loss

tfun/loss.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import global_config as global_cfg
 
+import pdb
+
 class loss(object):
     """
     This class work by adding loss values (computed from the net + target values) into the loss collection, named [lkey]
@@ -23,12 +25,12 @@ class loss(object):
 	:paran target: expected output
         :param target_weight: target target_weight, use to balance between classes/remove pad sammples
     	:param lm: param indicate the important rate of this loss comparing to others
-    	""" 
+    	"""
 	xdev = X - tf.reduce_max(X, keep_dims=True, reduction_indices=[-1])
         lsm = xdev - tf.log(tf.reduce_sum(tf.exp(xdev), keep_dims=True, reduction_indices=[-1]))
         if (target_weight == None):
             target_weight=1
-        l = -tf.reduce_mean(target_weight*target*lsm, name='softmax_log_loss')
+        l = -tf.reduce_sum(target_weight*target*lsm, name='softmax_log_loss')/tf.cast(tf.shape(X)[0], dtype=global_cfg.dtype)
         tf.add_to_collection(self.lkey, l)
         if (self.use_tboard):
             tf.summary.scalar('softmax_log_loss', l)