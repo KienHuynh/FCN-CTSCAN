4e24bcb7961d1fee7f3c0915b1c12fb7c87ae4c8
kien huynh <hckien@hcmut.edu.vn>
2017-04-11 09:39:13 +0700

    added fcn_pretrain.py

---
+++ fcn_pretrain.py
@@ -0,0 +1,11 @@
+from tfun.loss import loss
+from tfun.trainer import trainer
+from tfun.util import create_one_hot, create_conv_layer
+from tfun.global_config import global_cfg
+
+def create_classifier_dnn():
+    a = 2
+
+if __name__ == '__main__':
+    a = 1
+
