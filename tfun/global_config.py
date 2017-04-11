40e9d2ccfb66b64888404b37863a842fc2fce3e6
KienHuynh <hckien@hcmut.edu.vn>
2017-04-11 20:00:23 +0700

    added related variables for saving

--- tfun/global_config.py
+++ tfun/global_config.py
@@ -1,13 +1,21 @@
 import numpy as np
 
+dtype = np.float32
+device = '/gpu:0'
+train_dir = '../data/trained/'
 use_tboard = True
+
 lkey = 'loss' 
 batch_size = 0
 num_train = 0
 num_test = 0
 num_val = 0
-dtype = np.float32
-device = '/gpu:0'
-train_dir = '../data/trained/'
-max_step = 100000 
+
+max_step = 1000000 
 log_freq = 1
+val_freq = 1000
+
+save_freq = 100
+ckpt_name = 'model.ckpt'
+
+rng_seed = 1311
