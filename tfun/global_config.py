dcf6d3a4862e59188db7ed15b468e0831781a5d2
kien huynh <hckien@hcmut.edu.vn>
2017-04-11 14:56:30 +0700

    Added global_confg.py as a module

---
+++ tfun/global_config.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+use_tboard = True
+lkey = 'loss' 
+batch_size = 0
+num_train = 0
+num_test = 0
+num_val = 0
+dtype = np.float32
+device = '/gpu:0'
+train_dir = '../data/trained/'
+max_step = 100000 
+log_freq = 1
